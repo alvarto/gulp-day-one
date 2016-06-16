f091d1472035b135baa2b6bf31d3a0cc2bbbe2c2
黄炎 <huangyan@huangyandeMacBook-Pro.local>
2016-06-16 14:27:42 +0800

    Fix: first commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# gulp-day-one
+# Gulpfile Day One
+
+This is an empty gulp project based on our practice for the last 2 years.
+
+It does these stuff with normal build by tying `gulp`:
+
+- compile jade files to html
+- compile jade files to html and rename them
+- compile AMD javascript entries to one file (browserify)
+- copy third party files
+- compile less files
+- copy other files
+- look out for changes and do previous things again
+
+Could also does these manually:
+
+- `gulp svg`: combine svg files so you can inline them
+- `gulp sprite`: make sprites or @2x sprites (using [glue.py](https://github.com/jorgebastida/glue))
+- `gulp font`: subset font files based on the charecters needed (using [sfntly](https://github.com/googlei18n/sfntly))
+
+# Bathtub thoughts behind this
+
+When using gulp, you copy and paste gulpfile in between projects. What if we use same gulpfile for different projects (coz we lazy)? How do we organize folders? This project tries to answer these questions.
+
+Every task is formed as "folder A to folder B". Thus we extract common folder pattern to one file: `globs.js`.
+Tasks running order are organized by their dependencies, and we chose to take relatively fixed steps to do those stuff.
+
+## Folder pattern
+
+A relatively good dev folder pattern could be like this:
+
+/UPCOMING IMAGE PLACEHOLDER/
+
+And somehow our output folder pattern is forced to be like this:
+
+/UPCOMING IMAGE PLACEHOLDER/
+
+In order to form this kind of gulp folder pipeline, we need to extract folder pattern. This is where `node-glob` fails us. Pattern information are lost after the matching.
+I've found another way to express folder inputs and outputs, the express.js-route-like-way-of-expressing:
+
+```
+// source
+javascript/bundle/:entry.js
+// dist
+destination/resource/js/:entry.bundle.js
+```
+
+These infomation are stored in `/bin/globs.js`. Check them out.
+
+## Folders
+
+- `/bin/`: Whatever a workflow requires but a frontend page doesn't require, it's here. 
+- `/asset/`: Every assets but the bundled js files
+- `/bundle/`: The bundled js files. It's seprated from the `/asset/` because the base folder of `require()` is `__dirname`. To skip the ugliness of using a hell lot of `require("../../../common/stuff.js")` or `require(COMMON_PATH + "/common/stuff.js")`, we simply, put, them, here. In one place.
+- `/destination/`: Auto-generated target files lie here
+
---
+++ gulpfile.js
@@ -0,0 +1,210 @@
+// gulp part
+var gulp = require('gulp'),
+	$ = require('gulp-load-plugins')(),
+	fs = require('fs'),
+	runsequence = require("run-sequence"),
+	exec = require('child_process').exec,
+	eventstream = require('event-stream');
+
+// project sub modules
+var globs = require("./bin/globs"),
+	str = require('./bin/string'),
+	clean = require('./bin/gulp-clean'),
+	curryTask = require('./bin/gulp-builder').curryTask;
+
+// browserify part
+var browserify = require('browserify'),
+	sourceStream = require('vinyl-source-stream');
+
+// simple build/copy tasks
+gulp.task('module:copy', function() {
+	return gulp.src(globs.js.module.source)
+		.pipe(gulp.dest(globs.js.module.dist));
+});
+
+gulp.task('js:copy', curryTask(
+	globs.js.nonModule,
+	function(source, destination) {
+		return gulp.src(source)
+			.pipe(gulp.dest(destination));
+	}
+));
+
+gulp.task('less:build', function(callback) {
+	return gulp.src(globs.css.less)
+		.pipe($.plumber({
+			errorHandler: callback
+		}))
+		.pipe($.less({
+			paths: [
+				'.', globs.css.lessCommon, globs.css.includeImgCommon
+			]
+		}))
+		.pipe($.flatten())
+		.pipe($.plumber.stop())
+		.pipe(gulp.dest(globs.css.dist));
+});
+
+gulp.task('jade:build', function(callback) {
+	var tasks = [];
+	var jadeOpt = {
+		pretty: '\t',
+		basedir: globs.views.base,
+	};
+
+	gulp.src(globs.views.main)
+		.pipe($.plumber())
+		.pipe($.jade(jadeOpt))
+		.pipe($.plumber.stop())
+		.pipe($.flatten())
+		.pipe(gulp.dest(globs.views.dist))
+
+	eventstream.merge(tasks)
+		.on('end', callback);
+});
+
+// build tasks with namespace
+gulp.task('js:compile', function(callback) {
+	curryTask(
+		globs.js.build,
+		function(source, destination) {
+			return browserify({
+					entries: [source]
+				})
+				.bundle()
+				.on('error', callback)
+				.pipe(sourceStream(source))
+				.pipe($.flatten())
+				.pipe(gulp.dest(destination));
+		}
+	)(callback);
+});
+
+gulp.task('images:copy', curryTask(
+	globs.bitmap,
+	function(source, destination) {
+		return gulp.src(source)
+			.pipe(gulp.dest(destination));
+	}
+));
+
+gulp.task('groceries:copy', curryTask(
+	globs.groceries,
+	function(source, destination) {
+		return gulp.src(source, {
+				buffer: false,
+			})
+			.pipe(gulp.dest(destination));
+	}
+));
+
+// gulp tasks for commandline
+gulp.task('clean:frontend', function(callback) {
+	clean(globs.del.frontend, 'frontend', callback);
+});
+
+gulp.task('clean:backend', function(callback) {
+	clean(globs.backend.created, 'backend', callback);
+});
+
+gulp.task('clean:sprite', function(callback) {
+	clean(globs.del.sprite, 'sprite', callback);
+});
+
+gulp.task('clean:font', function(callback) {
+	clean(globs.del.font, 'sprite', callback);
+});
+
+gulp.task('clean:svg', function(callback) {
+	clean(globs.del.svg, 'sprite', callback);
+});
+
+gulp.task('clean', [
+	'clean:frontend',
+	'clean:backend',
+	'clean:sprite',
+	'clean:font',
+	'clean:svg',
+]);
+
+// preprocesser
+gulp.task('sprite', require('./bin/gulp-sprite'));
+gulp.task('font', require('./bin/gulp-font'));
+gulp.task('svg', require('./bin/gulp-svg'));
+
+gulp.task('pre', ['sprite', 'font', 'svg']);
+
+// a good task which runs them all
+gulp.task('refresh', function(callback) {
+	runsequence(
+		'clean', // clean all autocommitted stuff
+		'pre', // precompile 
+		'build', // build and set up backend & test env
+		'deploy', // deploy to backend
+		callback
+	);
+});
+
+// watch related gulp tasks
+gulp.task('build', [
+	'jade:build',
+	'js:copy',
+	'js:compile',
+	'images:copy',
+	'module:copy',
+	'less:build',
+	'groceries:copy',
+	'test'
+]);
+
+gulp.task('html:change', function() {
+	return gulp.src(globs.deploy.html)
+		.pipe($.changed('.cache', {
+			hasChanged: $.changed.compareSha1Digest
+		}))
+		.pipe(gulp.dest('.cache'))
+		.pipe($.livereload());
+});
+
+gulp.task('watch', ['build'], function(callback) {
+	var changedArr = [];
+
+	function push(s) {
+		changedArr.push(s);
+	}
+
+	function pop() {
+		while (changedArr.length > 0) {
+			var s = changedArr.pop();
+			$.livereload.changed(s);
+		}
+	}
+
+	gulp.watch(globs.views.watch, ['jade:build']);
+	gulp.watch(globs.deploy.html, ['html:change']);
+
+	gulp.watch(globs.css.watch, ['less:build', pop])
+		.on('change', push);
+
+	gulp.watch(globs.groceries.watch, ['groceries:copy', pop])
+		.on('change', push);
+
+	gulp.watch(globs.js.module.watch, ['module:copy', pop])
+		.on('change', push);
+
+	gulp.watch(globs.js.nonModule.watch, ['js:copy', pop])
+		.on('change', push);
+
+	gulp.watch(globs.js.build.watch, ['js:compile', pop])
+		.on('change', push);
+
+	gulp.watch(globs.bitmap.watch, ['images:copy', pop])
+		.on('change', push);
+
+	$.livereload.listen();
+
+	callback();
+});
+
+// and the default
+gulp.task('default', ['watch']);
